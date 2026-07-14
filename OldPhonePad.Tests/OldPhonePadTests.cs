8c8ea5070f9cc318b0948a0eb6b546daae1a1b29
Ilma Afrin <ilma1.afrin1@gmail.com>
2026-07-14 20:56:34 +0200

    Add validation unit tests

--- OldPhonePad.Tests/OldPhonePadTests.cs
+++ OldPhonePad.Tests/OldPhonePadTests.cs
@@ -28,4 +28,22 @@ public class OldPhonePadTests
 
         Assert.Equal("HELLO", result);
     }
+    [Fact]
+    public void Decode_ShouldThrowArgumentNullException_WhenInputIsNull()
+    {
+        Assert.Throws<ArgumentNullException>(() => Decoder.Decode(null!));
+    }
+
+    [Fact]
+    public void Decode_ShouldThrowArgumentException_WhenInputDoesNotEndWithHash()
+    {
+        Assert.Throws<ArgumentException>(() => Decoder.Decode("33"));
+    }
+
+    [Fact]
+    public void Decode_ShouldThrowArgumentException_WhenInputContainsInvalidCharacter()
+    {
+        Assert.Throws<ArgumentException>(() => Decoder.Decode("22A#"));
+    }
+
 }
